999cc5879d10251a5c333d0d8d719dd66251e3e0
DeployAI <deployai@example.com>
2025-07-24 14:40:03 +0000

    Import cloud object: ec2 i-000ee7b133d6f8c4e

---
+++ terraform/imported_resources.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "Admin_System_7Twenty_Prod" {
+  # Dummy values for import - will be replaced with actual values after import
+  ami           = "ami-dummy"
+  instance_type = "t2.micro"
+  
+  # Import will populate the actual values
+}
